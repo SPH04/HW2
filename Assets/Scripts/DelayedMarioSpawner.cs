abb78a62f3db8b61368cf1a8a6cb378310dd5838
SPH <molchanoff.stephan@gmail.com>
2020-12-18 01:10:12 +1000

    Fixed code problems

---
+++ Assets/Scripts/DelayedMarioSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DelayedMarioSpawner : MonoBehaviour
+{
+    [SerializeField] private float _delayTime;
+
+    private MarioInstantiator[] _spawners;
+
+    private void Start()
+    {
+        _spawners = FindObjectsOfType<MarioInstantiator>();
+        StartCoroutine(SpawnWithDelay());
+    }
+    private IEnumerator SpawnWithDelay()
+    {
+        while (true)
+        {
+            foreach (MarioInstantiator instantiator in _spawners)
+            {
+                instantiator.Spawn();
+                yield return new WaitForSeconds(_delayTime);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MarioInstantiator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MarioInstantiator : MonoBehaviour
+{
+    [SerializeField] private Mario _template;
+
+    public void Spawn() =>
+        Instantiate(_template, transform.position, Quaternion.identity);
+}
